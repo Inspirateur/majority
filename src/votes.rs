b050dd8adbb7e7420f0708814185394d343b4d45
Teo <teo.orthlieb@gmail.com>
2023-03-25 15:38:53 +0100

    made vote ranking works even with options with no votes

--- src/votes.rs
+++ src/votes.rs
@@ -12,6 +12,9 @@ impl MJVotes for Vec<usize> {
     /// the other are neighboring medians used for tie-breaking as defined by the Majority Judgement 
     /// https://en.wikipedia.org/wiki/Majority_judgment#Voting_process
     fn nth_median(&self, n: usize) -> usize {
+        if self.len() == 0 {
+            return 0;
+        }
         // TODO: use div_ceil when stabilized
         let med = (self.len() + 1) / 2 - 1;
         let i = (n + 1) / 2;
@@ -24,7 +27,9 @@ impl MJVotes for Vec<usize> {
 
     /// Orders a pair of votes vector, using nth_median(i) iteratively until a winner is established
     fn vote_cmp(&self, other: &Self) -> Ordering {
-        for i in 0..self.len().min(other.len()) {
+        // the 0th median is defined as 0 even if there is NO votes
+        // this is why we evaluate at least the 0th median when there is no votes 
+        for i in 0..self.len().min(other.len()).max(1) {
             let self_med= self.nth_median(i);
             let other_med = other.nth_median(i);
             let ord = self_med.cmp(&other_med);
